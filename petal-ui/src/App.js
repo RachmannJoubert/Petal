9e8b5e17bdedaf1d55983893c8bdb913e1ec22de
Karen Croston <karencroston@hotmail.co.uk>
2023-05-19 14:51:08 +0100

    passing username as state to home

--- petal-ui/src/App.js
+++ petal-ui/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/app.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignIn from "./components/SignIn";
@@ -8,28 +8,16 @@ import Home from "./components/Home";
 import SearchResults from "./components/SearchResults";
 
 function App() {
-  const toggleForm = (formName) => {
-    console.log(formName);
-  };
-
+  const [user, setUser] = useState({});
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<SignIn onFormSwitch={toggleForm} />} />
-          <Route
-            path="/signup"
-            element={<SignUp onFormSwitch={toggleForm} />}
-          />
-          <Route
-            path="/plantdetail"
-            element={<PlantDetail onFormSwitch={toggleForm} />}
-          />
-          <Route path="/home" element={<Home onFormSwitch={toggleForm} />} />
-          <Route
-            path="/searchresults"
-            element={<SearchResults onFormSwitch={toggleForm} />}
-          />
+          <Route path="/" element={<SignIn setUser={setUser} />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/plantdetail" element={<PlantDetail />} />
+          <Route path="/home" element={<Home user={user} />} />
+          <Route path="/searchresults" element={<SearchResults />} />
         </Routes>
       </Router>
     </div>
--- petal-ui/src/components/Home.js
+++ petal-ui/src/components/Home.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const HomePage = () => {
+const HomePage = ({ user }) => {
   const navigate = useNavigate();
   const handleButtonClick = () => {
     console.log("Button clicked!");
@@ -11,6 +11,7 @@ const HomePage = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <p>Hello {user}</p>
       <button onClick={handleButtonClick}>Click Me</button>
     </div>
   );
